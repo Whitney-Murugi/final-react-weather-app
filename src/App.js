4642558bb71c16ad7670c821382c2fd14e6f2e09
Whitney-Murugi <mmaitsimurugi@gmail.com>
2025-02-18 06:34:25 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <p>
+        This project was made by <a href="https://github.com/Whitney-Murugi" target="_blank" rel="noreferrer"> Whitney Mmaitsi </a> and is
+        <a href="https://github.com/Whitney-Murugi/final-react-weather-app" target="_blank" rel="noreferrer">
+           open-sourced
+        </a>
+      </p>
     </div>
   );
 }
